39d4716cac1348b55092cdb6a27cb7db4cf0f1c2
nicocau <niicolascauchois@gmail.com>
2019-10-04 15:13:47 +0200

    Ajoute l'intrus et les robots

--- src/Modele/Intrus.java
+++ src/Modele/Intrus.java
@@ -1,4 +1,26 @@
 package Modele;
 
 public class Intrus {
+    private Case caseActuel;
+    private StatusIntru statusIntru = StatusIntru.RECHERCHE;
+
+    public Intrus(Case caseActuel) {
+        this.caseActuel = caseActuel;
+    }
+
+    public Case getCaseActuel() {
+        return caseActuel;
+    }
+
+    public void setCaseActuel(Case caseActuel) {
+        this.caseActuel = caseActuel;
+    }
+
+    public StatusIntru getStatusIntru() {
+        return statusIntru;
+    }
+
+    public void setStatusIntru(StatusIntru statusIntru) {
+        this.statusIntru = statusIntru;
+    }
 }
--- src/Modele/Robot.java
+++ src/Modele/Robot.java
@@ -1,4 +1,26 @@
 package Modele;
 
 public class Robot {
+    private Case caseActuel;
+    private StatusRobo statusRobo = StatusRobo.CHASSE;
+
+    public Robot(Case caseActuel) {
+        this.caseActuel = caseActuel;
+    }
+
+    public Case getCaseActuel() {
+        return caseActuel;
+    }
+
+    public void setCaseActuel(Case caseActuel) {
+        this.caseActuel = caseActuel;
+    }
+
+    public StatusRobo getStatusRobo() {
+        return statusRobo;
+    }
+
+    public void setStatusRobo(StatusRobo statusRobo) {
+        this.statusRobo = statusRobo;
+    }
 }
