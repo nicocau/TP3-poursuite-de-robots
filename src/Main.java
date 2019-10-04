15f7a64691c1fb0d2e59b50a48406ee65b440723
nicocau <niicolascauchois@gmail.com>
2019-10-04 15:22:53 +0200

    Ajoute les constante au main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
+    public static final int TAILLE_X = 20;
+    public static final int TAILLE_Y = 30;
+    public static final int NB_ROBOTS = 3;
+    public static final int DISTANCE_VUE = 4;
+    public static final int DISTANCE_VUE_MESSAGE = 2;
+    public static final double POURCENTAGE_MUR = 0.1;
+
     public static void main(String[] args) {
 
     }
