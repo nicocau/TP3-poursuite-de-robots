66bb055308ee4796fb5aafee7a0b22a4fabe4644
nicocau <niicolascauchois@gmail.com>
2019-10-04 15:13:22 +0200

    Ajoute les cases

--- src/Modele/Case.java
+++ src/Modele/Case.java
@@ -1,4 +1,45 @@
 package Modele;
 
 public class Case {
+    private int x = -1;
+    private int y = -1;
+    private StatusCase statusCase = StatusCase.VIDE;
+    private boolean decouvert = false;
+
+    public Case(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public StatusCase getStatusCase() {
+        return statusCase;
+    }
+
+    public void setStatusCase(StatusCase statusCase) {
+        this.statusCase = statusCase;
+    }
+
+    public boolean isDecouvert() {
+        return decouvert;
+    }
+
+    public void setDecouvert(boolean decouvert) {
+        this.decouvert = decouvert;
+    }
 }
