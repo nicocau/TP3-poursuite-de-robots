c7aa45bff82635a79a3236686e3c75de2a75b19b
nicocau <niicolascauchois@gmail.com>
2019-10-19 22:58:15 +0200

    Ajoute les logs pour la mainvue

--- src/Vue/MainView.java
+++ src/Vue/MainView.java
@@ -2,6 +2,8 @@ package Vue;
 
 import Controleur.Controleur;
 import Controleur.TypeDeplacement;
+import Log.Logger;
+import Log.TypeLog;
 import Main.Main;
 import Modele.Case;
 import Modele.Robot;
@@ -65,6 +67,7 @@ public class MainView extends Application{
     }
 
     void construirePlateauJeu(Stage primaryStage) {
+        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Crée le plateau");
         this.troupe = new Group();
         this.scene = new Scene(troupe, width, height, Color.ANTIQUEWHITE);
         this.dessinEnvironnement();
@@ -72,26 +75,32 @@ public class MainView extends Application{
 
         DessinIntrus dessinIntrus = this.dessinIntru;
         MainView mainView = this;
+        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Crée les touche");
         scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
             public void handle(KeyEvent keyEvent){
                 switch (keyEvent.getCode()){
                     case UP:
                     case Z:
+                        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Déplace le joueur en haut");
                         Controleur.getInstance().Deplacement(TypeDeplacement.HAUT,Terrain.getInstance().getIntrus(), dessinIntrus, mainView);
                         break;
                     case DOWN:
                     case S:
+                        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Déplace le joueur en bas");
                         Controleur.getInstance().Deplacement(TypeDeplacement.BAS,Terrain.getInstance().getIntrus(), dessinIntrus, mainView);
                         break;
                     case LEFT:
                     case Q:
+                        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Déplace le joueur a gauche");
                         Controleur.getInstance().Deplacement(TypeDeplacement.GAUCHE,Terrain.getInstance().getIntrus(), dessinIntrus, mainView);
                         break;
                     case RIGHT:
                     case D:
+                        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Déplace le joueur a droit");
                         Controleur.getInstance().Deplacement(TypeDeplacement.DROITE,Terrain.getInstance().getIntrus(), dessinIntrus, mainView);
                         break;
                     case SPACE:
+                        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Clique sur espace");
                         Controleur.getInstance().PrendMsg(mainView, mainView.dessinIntru);
                         break;
                 }
@@ -101,8 +110,10 @@ public class MainView extends Application{
         primaryStage.setTitle("Poursuite de robots");
         primaryStage.setScene(scene);
         primaryStage.show();
+        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Ajoute la timeline");
         this.littleCycle = new Timeline(new KeyFrame(Duration.millis(Main.tempo),
                 event -> {
+                    Logger.getInstance().ajouteUneLigne(TypeLog.DEBUG, "nouveau ticke");
                     this.animation();
                 }));
         littleCycle.setCycleCount(Timeline.INDEFINITE);
@@ -114,6 +125,7 @@ public class MainView extends Application{
         MainView mainView = this;
         Terrain.getInstance().getRobots().forEach(robot -> {
             TypeDeplacement typeDeplacement = Controleur.getInstance().choseRobotMove(robot);
+            Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Anime le robot => robot:" + robot+"; deplacement: "+typeDeplacement);
             Controleur.getInstance().Deplacement(typeDeplacement, robot, mainView.getDessinRobots().get(i[0]), mainView);
             mainView.getDessinRobots().get(i[0]).setFill((robot.getStatusRobo() == StatusRobo.PATROUILLE) ? DessinPerssonage.couleurRobot : DessinPerssonage.couleurRobotChase);
             i[0] += 1;
@@ -122,13 +134,16 @@ public class MainView extends Application{
     }
 
     private void dessinEnvironnement() {
+        Logger.getInstance().ajouteUneLigne(TypeLog.DEBUG, "Dessine les case");
         for (Case caseTerain : Terrain.getInstance().getCases()) {
             DessinCase dessinCase = new DessinCase(caseTerain.getX(), caseTerain.getY(), caseTerain.getStatusCase());
             this.dessinCases.add(dessinCase);
             this.troupe.getChildren().add(dessinCase);
         }
+        Logger.getInstance().ajouteUneLigne(TypeLog.DEBUG, "Desine l'intrus");
         this.dessinIntru = new DessinIntrus(Terrain.getInstance().getIntrus().getCaseActuel().getX(), Terrain.getInstance().getIntrus().getCaseActuel().getY());
         this.troupe.getChildren().add(dessinIntru);
+        Logger.getInstance().ajouteUneLigne(TypeLog.DEBUG, "Dessine les robot");
         Terrain.getInstance().getRobots().forEach(robot -> {
             DessinRobots dessinRobots = new DessinRobots(robot.getCaseActuel().getX(), robot.getCaseActuel().getY());
             this.troupe.getChildren().add(dessinRobots);
