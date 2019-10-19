3f5d8a6049cdbcdf0518b6e1a20f241f35fc22d3
nicocau <niicolascauchois@gmail.com>
2019-10-19 22:50:15 +0200

    Ajoute les logs pour le terain

--- src/Modele/Terrain.java
+++ src/Modele/Terrain.java
@@ -29,6 +29,7 @@ public class Terrain {
             }
         }
 
+        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Ajoute les 4 sortie");
         Case sortie = null;
         int motierX = (int) (Main.TAILLE_X - 1) / 2;
         int motierY = (int) (Main.TAILLE_Y - 1) / 2;
@@ -45,6 +46,7 @@ public class Terrain {
         this.casesVide.remove(sortie);
         sortie.setStatusCase(StatusCase.SORTIE);
 
+        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Crée les murs");
         ArrayList<Case> casesAVirer = new ArrayList<Case>();
         this.casesVide.forEach(c -> {
             if (random.nextDouble() <= Main.POURCENTAGE_MUR) {
@@ -56,6 +58,7 @@ public class Terrain {
         });
         casesAVirer.forEach(c-> this.casesVide.remove(c));
 
+        Logger.getInstance().ajouteUneLigne(TypeLog.DEBUG, "Vérifie si il reste assez de plasse");
         if (this.casesVide.size() < Main.NB_ROBOTS+1+1) {
             try {
                 throw new Exception("Il n'y a pas assez de casse libre pour placer les robots et pour le joueur");
@@ -66,15 +69,20 @@ public class Terrain {
             }
         }
 
+        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Ajoute le message");
         Case caseMessage = this.casesVide.get(random.nextInt(this.casesVide.size()));
         caseMessage.setStatusCase(StatusCase.MESSAGE);
         this.casesVide.remove(caseMessage);
 
+
+        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Ajoute les robots");
         for (int i = 0; i < Main.NB_ROBOTS; i++) {
             Case caseRobot = this.casesVide.get(random.nextInt(this.casesVide.size()));
+            Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Ajoute un robot => case" + caseRobot);
             this.getRobots().add(new Robot(caseRobot));
         }
 
+        Logger.getInstance().ajouteUneLigne(TypeLog.INFO, "Ajoute l'intru'");
         Case caseIntru = this.casesVide.get(random.nextInt(this.casesVide.size()));
         this.intrus = new Intrus(caseIntru);
     }
