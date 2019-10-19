ebb1d1981158265251c75f4df238dc1fffd6232a
nicocau <niicolascauchois@gmail.com>
2019-10-19 20:38:26 +0200

    Ajoute les déplacement en diagonal

--- src/Controleur/TypeDeplacement.java
+++ src/Controleur/TypeDeplacement.java
@@ -4,7 +4,11 @@ public enum  TypeDeplacement {
     HAUT (0,-1),
     BAS (0,1),
     GAUCHE (-1,0),
-    DROITE (1,0);
+    DROITE(1, 0),
+    DIAGONAL_HAUT_GAUCHE(-1, -1),
+    DIAGONAL_HAUT_DROITE(1, -1),
+    DIAGONAL_BAS_GAUCHE(-1, 1),
+    DIAGONAL_BAS_DROITE(1, 1);
 
     private int x = 0;
     private int y = 0;
